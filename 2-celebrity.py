7072101b1e27641f37f0a48189810dfd05cbd6c3
Puneet Saini <puneet29saini@gmail.com>
2019-01-16 10:33:50 +0530

    Added solution to third puzzle to 2-celebrity.py

--- 2-celebrity.py
+++ 2-celebrity.py
@@ -1,7 +1,7 @@
 #Given a list of intervals when celebrities will be at the party
 #Output is the time that you want to go the party when the maximum number of
 #celebrities are still there.
-#Problem exercise 1 and 2:
+#Problem exercise 1, 2 and 3:
 
 def preprocessing(Time):
     """Returns important range of time in which the celebrities enter the room
@@ -11,6 +11,28 @@ def preprocessing(Time):
     impRange = list(set([a[0] for a in impRange]))
     impRange.sort()
     return impRange
+
+def bestTimeToPartyWeight(schedule):
+    """Prints the maximum weight fpr celebrities present in given the schedule
+    params: schedule {2D array containing entering and leaving time along with weight of each celebrity}"""
+
+    #Initializing the number of celebrities present previously and weight
+    #of celebrities at each important time interval
+    initial = 0
+    weight = {}
+    impRange = preprocessing(schedule)
+
+    for time in impRange:
+        weight[time] = initial
+        for r0, r1, w in schedule:
+            if(r0==time):
+                weight[time] += w
+            if(r1==time):
+                weight[time] -= w
+            initial = weight[time]
+    
+    res = max(weight, key=weight.get)
+    print(f"For this schedule of celebrities, you want to attend at {res} o'clock where the weight ofattending celebrities is {weight[res]} and maximum! ")
 
 def bestTimeToParty(schedule, ystart=False, yend=False):
     """Prints the maximum number of celebrities present in given schedule of celebrities(schedule)
@@ -47,4 +69,7 @@ if __name__ == "__main__":
             (9, 10), (10, 11), (10, 12), (11, 12)]
     sched2 = [(6.0, 8.0), (6.5, 12.0), (6.5, 7.0), (7.0, 8.0), (7.5, 10.0), (8.0, 9.0), 
               (8.0, 10.0), (9.0, 12.0), (9.5, 10.0), (10.0, 11.0), (10.0, 12.0), (11.0, 12.0)]
-    bestTimeToParty(sched2, 10.0, 12.0)
+    sched3 = [(6.0, 8.0, 2), (6.5, 12.0, 1), (6.5, 7.0, 2), (7.0, 8.0, 2), (7.5, 10.0, 3), (8.0, 9.0, 2), 
+              (8.0, 10.0, 1), (9.0, 12.0, 2), (9.5, 10.0, 4), (10.0, 11.0, 2), (10.0, 12.0, 3), (11.0, 12.0, 7)]
+
+    bestTimeToPartyWeight(sched3)
